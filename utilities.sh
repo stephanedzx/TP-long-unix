239491a635cadb093cf09778bc78558312c8f652
Nab-88 <nicolas.bernard38460@gmail.com>
2016-11-28 16:27:25 +0100

    ajout de la version de Nicolas (utilities)

--- utilities.sh
+++ utilities.sh
@@ -1,21 +1,5 @@
 #! /bin/sh
 html_head () {
-<<<<<<< HEAD
-    echo "<html>\n<head>\n<title>%s</titre>\n</head>\n<body>" "$1"
-}
-
-html_title () {
-    echo "<h1>$1</h1>"
-}
-
-html_tail () {
-    echo "</body>
-    </html>"
-}
-
-generate_img_fragment () {
-    echo "<img src="">"
-=======
     printf "%-20<html> \n <head> \n <title>$1 \n</head> \n <body>"
 }
 
@@ -25,5 +9,4 @@ html_title () {
 
 html_tail () {
     printf </body> \n </html>
->>>>>>> 708d87fbed81c8830a3321b3324aa8f2ace9491a
 }
